fbf01657982d8abdf22beebed2d315d0ff9b399a
Juvenal PENGELE <juvenal.pengele@gmail.com>
2025-01-31 06:06:28 -0500

    Integrate a form file

---
+++ src/components/Form.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<template>
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+    <button type="submit">Submit</button>
+  </form>
+</template>
